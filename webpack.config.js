7a4b8091ec0f634a502414e9af52d38a97ef486b
DonyZ <632304196@qq.com>
2016-12-02 20:29:30 +0800

    html-webpack-plugin
    前面的部分只是打包脚本，这一部分是将脚本插入到html页面中，这里用到了html-webpack-plugin这个插件，因为前面的项目，不知道这个module.exports怎么写，所以另外创建了一个分支，值得注意的是在使用插件时候需要注册，然后在module.plugins再new出来(个人理解就像构造函数一样)

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   // 入口
   entry: "./hello.js",
   // 输出的文件名
   output: {
     filename: 'bundle.js'
-  }
+  },
+  // 添加我们的插件 会自动生成一个html文件
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Hello Webpack'
+    })
+  ]
 };
